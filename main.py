a0b8cff88b57b7b36ce586bf895cf866e12a9063
chuanmingxi <1009898302@qq.com>
2023-04-13 10:43:26 +0800

    implement model and testbench

---
+++ main.py
@@ -0,0 +1,90 @@
+from model import *
+import enum
+import matplotlib.pyplot as plt
+
+class MODEL_INPUT(enum.Enum):
+    FIVE_DAYS = 1
+    TWENTY_DAYS = 2
+
+class MODEL_OUTPUT(enum.Enum):
+    ONE_DAY = 1
+    FIVE_DAYS = 2
+
+def load_data(input_type = MODEL_INPUT.FIVE_DAYS, output_type = MODEL_OUTPUT.ONE_DAY, batch_size = 32):
+    # randomly generate data of images in gray scale
+    # if input_type == MODEL_INPUT.FIVE_DAYS:
+    # the input is a picture of 15*32 pixels
+    # the output is a float number
+    # if input_type == MODEL_INPUT.TWENTY_DAYS:
+    # the input is a picture of 60*64 pixels
+    # the output is a float number
+    if input_type == MODEL_INPUT.FIVE_DAYS:
+        train_dataset = torch.utils.data.TensorDataset(torch.rand(batch_size, 1, 32, 15), torch.rand(batch_size, 1))
+        val_dataset = torch.utils.data.TensorDataset(torch.rand(batch_size, 1, 32, 15), torch.rand(batch_size, 1))
+        test_dataset = torch.utils.data.TensorDataset(torch.rand(batch_size, 1, 32, 15), torch.rand(batch_size, 1))
+    elif input_type == MODEL_INPUT.TWENTY_DAYS:
+        train_dataset = torch.utils.data.TensorDataset(torch.rand(batch_size, 1, 64, 60), torch.rand(batch_size, 1))
+        val_dataset = torch.utils.data.TensorDataset(torch.rand(batch_size, 1, 64, 60), torch.rand(batch_size, 1))
+        test_dataset = torch.utils.data.TensorDataset(torch.rand(batch_size, 1, 64, 60), torch.rand(batch_size, 1))
+
+    # Data loader
+    train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader
+    
+
+
+def twenty_days():
+    # load data
+    train_loader, val_loader, test_loader = load_data(MODEL_INPUT.TWENTY_DAYS, MODEL_OUTPUT.ONE_DAY)
+    # show example data
+    for i, (inputs, labels) in enumerate(train_loader):
+        # plot 4 images as gray scale
+        plt.subplot(221)
+        plt.imshow(inputs[0][0], cmap=plt.get_cmap('gray'))
+        plt.subplot(222)
+        plt.imshow(inputs[1][0], cmap=plt.get_cmap('gray'))
+        plt.subplot(223)
+        plt.imshow(inputs[2][0], cmap=plt.get_cmap('gray'))
+        plt.subplot(224)
+        plt.imshow(inputs[3][0], cmap=plt.get_cmap('gray'))
+        # show the plot
+        plt.show()
+        break
+
+
+    # train model
+    model = CNN20d()
+    train_model(model, train_loader, val_loader, num_epochs=10, learning_rate=0.001, batch_size=32, device='cpu', weight_decay=0.0)
+    test_model(model, test_loader, device='cpu')
+
+def five_days():
+    # load data
+    train_loader, val_loader, test_loader = load_data(MODEL_INPUT.FIVE_DAYS, MODEL_OUTPUT.ONE_DAY)
+    # show example data
+    for i, (inputs, labels) in enumerate(train_loader):
+        # plot 4 images as gray scale
+        plt.subplot(221)
+        plt.imshow(inputs[0][0], cmap=plt.get_cmap('gray'))
+        plt.subplot(222)
+        plt.imshow(inputs[1][0], cmap=plt.get_cmap('gray'))
+        plt.subplot(223)
+        plt.imshow(inputs[2][0], cmap=plt.get_cmap('gray'))
+        plt.subplot(224)
+        plt.imshow(inputs[3][0], cmap=plt.get_cmap('gray'))
+        # show the plot
+        plt.show()
+        break
+
+
+    # train model
+    model = CNN5d()
+    train_model(model, train_loader, val_loader, num_epochs=10, learning_rate=0.001, batch_size=32, device='cpu', weight_decay=0.0)
+    test_model(model, test_loader, device='cpu')
+
+
+if __name__ == '__main__':
+    five_days()
+    twenty_days()
---
+++ model.py
@@ -0,0 +1,124 @@
+# each day is displayed in 3 pixels
+# use cnn to predict 
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+class CNN5d(nn.Module):
+    # input in shape of N*15*32
+    # output in shape of N*1
+    def __init__(self):
+        super(CNN5d, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16 * 4 * 3, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+    
+class CNN20d(nn.Module):
+    # input in shape of N*60*64
+    # output in shape of N*1
+    # with 3 conv layers
+    def __init__(self):
+        super(CNN20d, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 3)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.conv3 = nn.Conv2d(16, 32, 3)
+        self.fc1 = nn.Linear(32 * 5 * 6, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 1)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+    def num_flat_features(self, x):
+        size = x.size()[1:]
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+def train_model(model, train_loader, val_loader, num_epochs=10, learning_rate=0.001, batch_size=32, device='cpu', weight_decay=0.0):
+    model = model.to(device)
+    # train model
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+    loss_func = nn.MSELoss().to(device)
+    for epoch in range(num_epochs):
+        # training
+        model.train()
+        for i, data in enumerate(train_loader):
+            inputs, labels = data
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = loss_func(outputs, labels)
+            loss.backward()
+            optimizer.step()
+        print('| Epoch: %d | Loss: %.4f |' % (epoch, loss.item()))
+
+        # validation
+        v_loss = 0
+        v_accu = 0
+        model.eval()
+        for i, data in enumerate(val_loader):
+            inputs, labels = data
+            inputs = inputs.float().to(device)
+            labels = labels.float().to(device)
+            outputs = model(inputs)
+            loss = loss_func(outputs, labels)
+            v_loss += loss.item()
+            v_accu += torch.sum(torch.abs(outputs - labels) < 0.5).item()
+        v_loss /= len(val_loader)
+        v_accu /= len(val_loader) * batch_size
+        print('| Validation Loss: %.4f | Accuracy: %.4f |' % (v_loss, v_accu))
+
+    return model
+
+def test_model(model, test_loader, batch_size=32, device='cpu'):
+    model = model.to(device)
+    loss_func = nn.MSELoss().to(device)
+    model.eval()
+    t_loss = 0
+    t_accu = 0
+    for i, data in enumerate(test_loader):
+        inputs, labels = data
+        inputs = inputs.float().to(device)
+        labels = labels.float().to(device)
+        outputs = model(inputs)
+        loss = loss_func(outputs, labels)
+        t_loss += loss.item()
+        t_accu += torch.sum(torch.abs(outputs - labels) < 0.5).item()
+    t_loss /= len(test_loader)
+    t_accu /= len(test_loader) * batch_size
+    print('| Test Loss: %.4f | Accuracy: %.4f |' % (t_loss, t_accu))
