9ef8e1cadbf42a9520400c1136916996c6fb9267
Richard Song <song-s20@mails.tsinghua.edu.cn>
2023-04-21 03:44:05 +0000

    finish inference & test, needed to be test

---
+++ inference.py
@@ -0,0 +1,79 @@
+from importlib import reload
+from model import *
+from train import *
+from dataset import *
+import dataset as _D
+reload(_D)
+import utils as _U
+reload(_U)
+import yaml
+import argparse
+
+parser = argparse.ArgumentParser(description='Train Models via YAML files')
+parser.add_argument('setting', type=str, \
+                    help='Experiment Settings, should be yaml files like those in /configs')
+
+args = parser.parse_args()
+
+with open(args.setting, 'r') as f:
+    setting = _U.Dict2ObjParser(yaml.safe_load(f)).parse()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+assert setting.MODEL in ['CNN5d', 'CNN20d'], f"Wrong Model Template: {setting.MODEL}"
+
+if 'factors' not in os.listdir('./'):
+    os.system('mkdir factors')
+if setting.INFERENCE.FACTORS_SAVE_FILE.split('/')[1] not in os.listdir('./factors/'):
+    os.system(f"cd factors && mkdir {setting.INFERENCE.FACTORS_SAVE_FILE.split('/')[1]}")
+
+
+def model_inference(model):
+    
+    model.eval()
+    # iterate over test data
+    sub_points = [setting.TEST.START_DATE] + [int(setting.TEST.END_DATE//1e4 * 1e4) + i*100 + 1 for i in range(4, 13, 3)] + [setting.TEST.END_DATE]
+
+    symbol_factors = pd.DataFrame([], index=['code', 'date', 'up_factor']).T
+    for m_idx in range(len(sub_points)-1):
+        print(f"Inferencing: {sub_points[m_idx]} - {sub_points[m_idx+1]}")
+        
+        inference_dataset = _D.ImageDataSet(win_size = setting.DATASET.LOOKBACK_WIN, \
+                                        start_date = sub_points[m_idx], \
+                                        end_date = sub_points[m_idx+1], \
+                                        mode = 'inference', \
+                                        indicators = setting.DATASET.INDICATORS, \
+                                        show_volume = setting.DATASET.SHOW_VOLUME, \
+                                        parallel_num=setting.DATASET.PARALLEL_NUM)
+        inference_imageset = inference_dataset.generate_images(1.0)
+
+        for id in range(len(inference_imageset)-1):
+            if len(inference_dataset[id][1]) == 0:
+                continue
+            inference_imgs = []
+            for img in inference_dataset[id][1]:
+                inference_imgs.append(img[0])
+            input = torch.Tensor(np.array(inference_imgs))
+            input = input.to(device)
+            output = model(input)[:, 1]
+            up_factors = []
+            for pred in output:
+                up_factors.append(pred.item())
+            symbol_f = pd.DataFrame([[inference_dataset[id][0] for _ in range(len(inference_dataset[id][1]))], inference_dataset[id][2], up_factors], index=['code', 'date', 'up_factor']).T
+            
+            symbol_factors = pd.concat([symbol_factors, symbol_f], axis=0)
+            
+    return symbol_factors
+
+
+if __name__ == '__main__':
+    if setting.MODEL == 'CNN5d':
+        model = CNN5d()
+    else:
+        model = CNN20d()
+    model.to(device)
+
+    state_dict = torch.load(setting.TRAIN.MODEL_SAVE_FILE)
+    model.load_state_dict(state_dict['model_state_dict'])
+
+    factors = model_inference(model)
+    factors.to_csv(setting.INFERENCE.FACTORS_SAVE_FILE)
--- run.ps1
+++ run.ps1
@@ -19,8 +19,8 @@ Write-Host "train finishes at $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
 git add .
 git commit -m "train finished at $(Get-Date -Format "yyyy-MM-dd HH:mm:ss")"
 git push
-# get auth use ./OneDriveUploader.exe -ms -a "url"
-# where url is get here: https://link.zhihu.com/?target=https%3A//login.microsoftonline.com/common/oauth2/v2.0/authorize%3Fclient_id%3D78d4dc35-7e46-42c6-9023-2d39314433a5%26response_type%3Dcode%26redirect_uri%3Dhttp%3A//localhost/onedrive-login%26response_mode%3Dquery%26scope%3Doffline_access%2520User.Read%2520Files.ReadWrite.All
-# access this web page and allow the app to access your onedrive, then copy the url that's it
-# after auth, run this command to upload files
-./OneDriveUploader.exe -c ./auth.json -s "logs" -r "log"
+# # get auth use ./OneDriveUploader.exe -ms -a "url"
+# # where url is get here: https://link.zhihu.com/?target=https%3A//login.microsoftonline.com/common/oauth2/v2.0/authorize%3Fclient_id%3D78d4dc35-7e46-42c6-9023-2d39314433a5%26response_type%3Dcode%26redirect_uri%3Dhttp%3A//localhost/onedrive-login%26response_mode%3Dquery%26scope%3Doffline_access%2520User.Read%2520Files.ReadWrite.All
+# # access this web page and allow the app to access your onedrive, then copy the url that's it
+# # after auth, run this command to upload files
+# ./OneDriveUploader.exe -c ./auth.json -s "logs" -r "log"
