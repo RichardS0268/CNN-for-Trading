8790f866422ba5ae43da3bd55f7a2e6126bafd30
Richard Song <song-s20@mails.tsinghua.edu.cn>
2023-04-25 02:22:43 +0000

    last training

--- build.sh
+++ build.sh
@@ -2,28 +2,6 @@
 
 FILES1=$(find configs/I20R5 -type f -name '*.yml')
 
-for f in $FILES1
-do
-  python main.py $f
-
-  current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-  git add .
-  git commit -m "Finished $f at $current_time"
-  git push origin main
-done
-
-FILES2=$(find configs/I5R20 -type f -name '*.yml')
-
-for f in $FILES2
-do
-  python main.py $f
-
-  current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-  git add .
-  git commit -m "Finished $f at $current_time"
-  git push origin main
-done
-
 FILES3=$(find configs/I5R5 -type f -name '*.yml')
 
 for f in $FILES3
